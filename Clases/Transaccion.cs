400a5261a700d2d8ac78a165f8ff0af392257f82
Adrian Campos <jradriancampos@outlook.com>
2026-05-18 22:45:04 -0500

    Gran cambio realizado, se agrego la clase Transacciones que contiene la informacion de cada transaccion realizada y se agrego la migracion para el EF de dicha clase, tambien se instalo la base de datos en la misma carpeta del proyecto para portabilidad entre maquinas, solo que para poder tener una ejecucion eficiente se necesita que se tenga el localdb instalado en su maquina, y la misma db tiene nueva informacion con respecto a transaccion e inventario.

---
+++ Clases/Transaccion.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IlkaPoint.Clases
+{
+    public class Transaccion
+    {
+        [Key] //AQUI INDICA QUE LA LLAVE PARA QUE EL EF LO RECONOZCA AL CREAR SUS TABLAS RELACIONALES 
+        public int idTransaccion { get; set; }
+
+        private DateTime fecha;
+        private string metodoPago;
+        private bool esJubilado;
+        private decimal total;
+
+        //Todos los atributos necesitan get y set en su encapsulamiento
+        private DateTime Fecha
+        {
+            get
+            {
+                return fecha;
+            }
+
+            set
+            {
+                fecha = value;
+            }
+        }
+
+        private string MetodoPago
+        {
+            get
+            {
+                return metodoPago;
+            }
+
+            set
+            {
+                metodoPago = value;
+            }
+        }
+
+        private bool EsJubilado
+        {
+            get
+            {
+                return esJubilado;
+            }
+
+            set
+            {
+                esJubilado = value;
+            }
+        }
+        
+        private decimal Total
+        {
+            get
+            {
+                return total;
+            }
+
+            set
+            {
+                total = value;
+            }
+        }
+
+        //Constructor
+        public Transaccion(string metodoPago, bool esJubilado) //registrarTransaccion()
+        {
+            this.metodoPago = metodoPago;
+            this.esJubilado = esJubilado;
+
+            fecha = DateTime.Now; //Cada vez que alguien haga una transaccion, la fecha va a ser hoy.
+
+
+            total = CalcularDescuento();
+        }
+
+        private decimal CalcularDescuento() //Funcion privada por que se llama solo en la clase, un forms no llamara a la funcion
+        {
+            decimal descuento = Convert.ToDecimal(Convert.ToDouble(total) * 0.15);
+            decimal resultado;
+            if (esJubilado)
+            {
+                resultado = total - descuento;
+            }
+            else
+            {
+                resultado = total;
+            }
+            return resultado;
+        }
+    }
+}
---
+++ Migrations/202605190339057_AgregarTransaccion.cs
@@ -0,0 +1,25 @@
+﻿namespace IlkaPoint.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AgregarTransaccion : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Transaccions",
+                c => new
+                    {
+                        idTransaccion = c.Int(nullable: false, identity: true),
+                    })
+                .PrimaryKey(t => t.idTransaccion);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Transaccions");
+        }
+    }
+}
